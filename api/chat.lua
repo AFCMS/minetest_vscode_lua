ff80ad85e3b578621ede5f4fe922e4f1d0659db1
AFCMS <afcm.contact@gmail.com>
2022-05-12 17:06:18 +0200

    Add chat functions documentation

--- api/chat.lua
+++ api/chat.lua
@@ -1,14 +1,22 @@
 
----comment
+---Send a chat message to all players
 ---@param text string
 function minetest.chat_send_all(text) end
 
----comment
+---Send a chat message to player `name`
 ---@param name string
 ---@param text string
 function minetest.chat_send_player(name, text) end
 
----comment
+---Used by the server to format a chat message, based on the setting `chat_message_format`.
+---
+---Refer to the documentation of the setting for a list of valid placeholders.
+---
+---Takes player name and message, and returns the formatted string to be sent to players.
+---
+---Can be redefined by mods if required, for things like colored names or messages.
+---
+---**Only** the first occurrence of each placeholder will be replaced.
 ---@param name string
 ---@param message string
 function minetest.format_chat_message(name, message) end
