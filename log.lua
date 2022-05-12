f33dbea9927e8fc6e7103fbe429eb6e41792d319
AFCMS <afcm.contact@gmail.com>
2022-05-12 17:46:34 +0200

    Add log API definition

---
+++ log.lua
@@ -0,0 +1,11 @@
+---@alias log_level '"none"'|'"error"'|'"warning"'|'"action"'|'"info"'|'"verbose"'
+
+---Log a message.
+---@overload fun(message: string)
+---@param level log_level Default: `none`
+---@param message string
+function minetest.log(level, message) end
+
+---Equivalent to `minetest.log(table.concat({...}, "\t"))`
+---@vararg string
+function minetest.debug(...) end
