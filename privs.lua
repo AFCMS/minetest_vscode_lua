5215d895b3d3301869b25ccba207957d8d8f342d
AFCMS <afcm.contact@gmail.com>
2022-05-23 22:26:04 +0200

    document privs registration

--- privs.lua
+++ privs.lua
@@ -1,9 +1,36 @@
 ---@class privilege_definition
----@field description string
----@field give_to_singleplayer boolean
----@field give_to_admin boolean
----@field on_grant fun(name: string, granter_name: string): boolean
----@field on_revoke fun(name: string, revoker_name: string): boolean
+local privilege_definition = {}
+
+---Privilege description.
+---@type string
+privilege_definition.description = nil
+
+---Whether to grant the privilege to singleplayer.
+---@type boolean
+privilege_definition.give_to_singleplayer = nil
+
+---Whether to grant the privilege to the server admin.
+---
+---Uses value of 'give_to_singleplayer' by default.
+---@type boolean
+privilege_definition.give_to_admin = nil
+
+---Called when given to player 'name' by 'granter_name'.
+---
+---'granter_name' will be nil if the priv was granted by a mod.
+---
+---Return true to stop `register_on_priv_grant` from being called.
+---@type fun(name: string, granter_name?: string): boolean
+privilege_definition.on_grant = nil
+
+---Called when taken from player 'name' by 'revoker_name'.
+---
+---'revoker_name' will be nil if the priv was revoked by a mod.
+---
+---Return true to stop `register_on_priv_revoke` from being called.
+---@type fun(name: string, revoker_name?: string): boolean
+privilege_definition.on_revoke = nil
+
 
 ---Register a new privilege.
 ---
