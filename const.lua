6989462cc52fd73e4e12fa3796851111b66291ef
AFCMS <afcm.contact@gmail.com>
2022-06-01 11:29:44 +0200

    add some more constants

--- const.lua
+++ const.lua
@@ -1,2 +1,10 @@
 minetest.PLAYER_MAX_HP_DEFAULT = 20
 minetest.PLAYER_MAX_BREATH_DEFAULT = 20
+
+minetest.LIGHT_MAX = 14
+
+minetest.EMERGE_CANCELLED = 0
+minetest.EMERGE_ERRORED = 1
+minetest.EMERGE_FROM_MEMORY = 2
+minetest.EMERGE_FROM_DISK = 3
+minetest.EMERGE_GENERATED = 4
