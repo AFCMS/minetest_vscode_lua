91d43baaa5369ca0d222b66b3dad18b3800afe66
AFCMS <afcm.contact@gmail.com>
2022-06-03 12:33:35 +0200

    Add `DIR_DELIM` and `PLATFORM` constants

--- const.lua
+++ const.lua
@@ -8,3 +8,9 @@ minetest.EMERGE_ERRORED = 1
 minetest.EMERGE_FROM_MEMORY = 2
 minetest.EMERGE_FROM_DISK = 3
 minetest.EMERGE_GENERATED = 4
+
+---@type '"/"'|'"\\\\"'
+DIR_DELIM = nil
+
+---@type '"Android"'|'"Linux"'|'"Windows"'|'"BSD"'|'"OSX"'|'"iOS"'|'"Apple"'|'"AIX"'|'"HP-UX"'|'"Solaris"'|'"SunOS"'|'"Haiku"'|'"Cygwin"'|'"Posix"'|'"Unix"'|'"?"'
+PLATFORM = nil
