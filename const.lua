32f382b7d9d69fd644508df53d7e8efdfe953480
AFCMS <afcm.contact@gmail.com>
2022-06-04 11:07:27 +0200

    Add some more constants

--- const.lua
+++ const.lua
@@ -1,16 +1,47 @@
-minetest.PLAYER_MAX_HP_DEFAULT = 20
-minetest.PLAYER_MAX_BREATH_DEFAULT = 20
+-- Builtin constants
 
+---Built-in Content ID of uknown node (for use with VoxelManip API)
+minetest.CONTENT_UNKNOWN = 125
+
+---Built-in Content ID of air node (for use with VoxelManip API)
+minetest.CONTENT_AIR = 126
+
+---Built-in Content ID of ignore node (for use with VoxelManip API)
+minetest.CONTENT_IGNORE = 127
+
+---Default maximal HP of a player
+minetest.PLAYER_MAX_HP_DEFAULT = 20
+
+---Default maximal breath of a player
+minetest.PLAYER_MAX_BREATH_DEFAULT = 10
+
+
+
+---Maximum value for node `"light_source"` parameter
 minetest.LIGHT_MAX = 14
 
+
+
+---Block emerge status constants (for use with minetest.emerge_area)
 minetest.EMERGE_CANCELLED = 0
+
+---Block emerge status constants (for use with minetest.emerge_area)
 minetest.EMERGE_ERRORED = 1
+
+---Block emerge status constants (for use with minetest.emerge_area)
 minetest.EMERGE_FROM_MEMORY = 2
+
+---Block emerge status constants (for use with minetest.emerge_area)
 minetest.EMERGE_FROM_DISK = 3
+
+---Block emerge status constants (for use with minetest.emerge_area)
 minetest.EMERGE_GENERATED = 4
 
+
+---Directory separator in use on the system.
 ---@type '"/"'|'"\\\\"'
 DIR_DELIM = nil
 
+---Platform Minetest was build for
 ---@type '"Android"'|'"Linux"'|'"Windows"'|'"BSD"'|'"OSX"'|'"iOS"'|'"Apple"'|'"AIX"'|'"HP-UX"'|'"Solaris"'|'"SunOS"'|'"Haiku"'|'"Cygwin"'|'"Posix"'|'"Unix"'|'"?"'
 PLATFORM = nil
