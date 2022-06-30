63b2deeaf5de2a2a8f47cf69edb878017039f849
AFCMS <afcm.contact@gmail.com>
2022-07-01 00:51:37 +0200

    Make minetest namespace a class like other lua modules

--- minetest.lua
+++ minetest.lua
@@ -1,7 +1,11 @@
+---@diagnostic disable: lowercase-global
+
 ---Minetest 7ffc0268 API
 ---
 ---[Documentation](https://github.com/minetest/minetest/blob/7ffc0268/doc/lua_api.txt)
+---@class minetest
 minetest = {}
 
 ---@deprecated
-core = minetest
+---@type minetest
+core = {}
