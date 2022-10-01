e75e4d413e2d146dc3f07a62163b7013d32a0dcc
AFCMS <afcm.contact@gmail.com>
2022-10-01 22:56:47 +0200

    `minetest.get_inventory` + detached inventories

--- classes/invref.lua
+++ classes/invref.lua
@@ -97,3 +97,44 @@ function InvRef:remove_item(listname, stack) end
 ---Returns a location compatible with `minetest.get_inventory(location)`.
 ---@return inventory_location
 function InvRef:get_location() end
+
+---Get an inventory.
+---@param location {type: '"player"', name: string}|{type: '""', node: Vector}|{type: '"detached"', name: string}
+---@return InvRef?
+function minetest.get_inventory(location) end
+
+---@class detached_inventory_callbacks
+---Called when a player wants to move items inside the inventory.
+---
+---Return value: number of items allowed to move.
+---@field allow_move fun(inv: InvRef, from_list: string, from_index: integer, to_list: string, to_index: integer, count: integer, player: ObjectRef): integer
+---Called when a player wants to put something into the inventory.
+---
+---Return value: number of items allowed to put.
+---
+---Return value -1: Allow and don't modify item count in inventory.
+---@field allow_put fun(inv: InvRef, listname: string, index: integer, stack: ItemStack, player: ObjectRef): integer
+---Called when a player wants to take something out of the inventory.
+---
+---Return value: number of items allowed to take.
+---
+---Return value -1: Allow and don't modify item count in inventory.
+---@field allow_take fun(inv: InvRef, listname: string, index: integer, stack: ItemStack, player: ObjectRef): integer
+---@field on_move fun(inv: InvRef, from_list: string, from_index: integer, to_list: string, to_index: integer, count: integer, player: ObjectRef)
+---@field on_put fun(inv: InvRef, listname: string, index: integer, stack: ItemStack, player: ObjectRef)
+---@field on_take fun(inv: InvRef, listname: string, index: integer, stack: ItemStack, player: ObjectRef)
+
+
+---Create a detached inventory.
+---@param name string
+---@param callbacks detached_inventory_callbacks
+---@param player_name? string Make detached inventory available to one player exclusively, by default they will be sent to every player (even if not used). Note that this parameter is mostly just a workaround and will be removed in future releases.
+---@return InvRef
+function minetest.create_detached_inventory(name, callbacks, player_name) end
+
+---Remove a detached inventory.
+---
+---Returns a boolean indicating whether the removal succeeded.
+---@param name string
+---@return boolean
+function minetest.remove_detached_inventory(name) end
