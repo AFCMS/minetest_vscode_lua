baecdbb17b8053870d33af8a5ed1e0ac73af1086
AFCMS <afcm.contact@gmail.com>
2022-10-02 16:48:08 +0200

    Object Properties Documentation

--- classes/objectref.lua
+++ classes/objectref.lua
@@ -151,38 +151,149 @@ function ObjectRef:get_bone_position(bone) end
 
 ---Object Properties
 ---@class object_properties
+---**Player Only**
+---
+---Defaults to `minetest.PLAYER_MAX_HP_DEFAULT`.
 ---@field hp_max integer
+---**Player Only**
+---
+---Defaults to `minetest.PLAYER_MAX_BREATH_DEFAULT`.
 ---@field breath_max integer
+---**Player Only**
+---
+---Zoom FOV in degrees.
+---
+---Note that zoom loads and/or generates world beyond the server's maximum send and generate distances, so acts like a telescope.
+---
+---Smaller zoom_fov values increase the distance loaded/generated.
+---
+---Defaults to 15 in creative mode, 0 in survival mode.
+---
+---`zoom_fov = 0` disables zooming for the player.
 ---@field zoom_fov number
+---**Player Only**
+---
+---Camera height above feet position in nodes.
+---
+---Defaults to 1.625
 ---@field eye_height number
+---Collide with `walkable` nodes.
 ---@field physical boolean
+---Collide with other objects if `physical = true`
 ---@field collide_with_objects boolean
+---`{xmin, ymin, zmin, xmax, ymax, zmax}` in nodes from object position.
 ---@field collisionbox number[]
+---Selection box uses collision box dimensions when not set.
+---
+---`{xmin, ymin, zmin, xmax, ymax, zmax}` in nodes from object position.
 ---@field selectionbox number[]
+---Overrides selection box when `false`.
 ---@field pointable boolean
+---* `"cube"` is a node-sized cube.
+---* `"sprite"` is a flat texture always facing the player.
+---* `"upright_sprite"` is a vertical flat texture.
+---* `"mesh"` uses the defined mesh model.
+---* `"wielditem"` is used for dropped items. (see builtin/game/item_entity.lua).
+---   * For this use 'wield_item = itemname' (Deprecated: 'textures = {itemname}').
+---   * If the item has a 'wield_image' the object will be an extrusion of that, otherwise:
+---      * If 'itemname' is a cubic node or nodebox the object will appear identical to 'itemname'.
+---      * If 'itemname' is a plantlike node the object will be an extrusion of its texture.
+---   * Otherwise for non-node items, the object will be an extrusion of 'inventory_image'.
+---   * If 'itemname' contains a ColorString or palette index (e.g. from `minetest.itemstring_with_palette()`), the entity will inherit the color.
+---* `"item"` is similar to `"wielditem"` but ignores the 'wield_image' parameter.
 ---@field visual '"cube"'|'"sprite"'|'"upright_sprite"'|'"mesh"'|'"wielditem"'|'"item"'
----@field visual_size Vector
+---Multipliers for the visual size.
+---
+---If `z` is not specified, `x` will be used to scale the entity along both horizontal axes.
+---@field visual_size Vector|{x: number, y: number}
+---File name of mesh when using `"mesh"` visual.
 ---@field mesh string
+---Number of required textures depends on visual.
+---* `"cube"` uses 6 textures just like a node, but all 6 must be defined.
+---* `"sprite"` uses 1 texture.
+---* `"upright_sprite"` uses 2 textures: {front, back}.
+---* `"wielditem"` expects 'textures = {itemname}' (see 'visual' above).
+---* `"mesh"` requires one texture for each mesh buffer/material (in order)
 ---@field textures string[]
+---Number of required colors depends on visual.
 ---@field colors table
+---Use texture's alpha channel.
+---
+---Excludes `"upright_sprite"` and `"wielditem"`.
+---
+---Note: currently causes visual issues when viewed through other semi-transparent materials such as water.
 ---@field use_texture_alpha boolean
+---Used with spritesheet textures for animation and/or frame selection according to position relative to player.
+---
+---Defines the number of columns and rows in the spritesheet: `{x = columns, y = rows}`.
 ---@field spritediv {x: integer, y: integer}
+---Used with spritesheet textures.
+---
+---Defines the `{x = column, y = row}` position of the initially used frame in the spritesheet.
 ---@field initial_sprite_basepos {x: integer, y: integer}
+---If `false`, object is invisible and can't be pointed.
 ---@field is_visible boolean
+---If `true`, is able to make footstep sounds of nodes (see node sound definition for details).
 ---@field makes_footstep_sound boolean
+---Set constant rotation in radians per second, positive or negative.
+---
+---Object rotates along the local Y-axis, and works with set_rotation.
+---
+---Set to 0 to disable constant rotation.
 ---@field automatic_rotate number
+---If positive number, object will climb upwards when it moves horizontally against a `walkable` node, if the height difference is within `stepheight`.
 ---@field stepheight number
+---Automatically set yaw to movement direction, offset in degrees.
+---
+---`false` to disable.
 ---@field automatic_face_movement_dir number
+---Limit automatic rotation to this value in degrees per second.
+---
+---No limit if value <= 0.
 ---@field automatic_face_movement_max_rotation_per_sec number
+---Set to false to disable backface_culling for model.
 ---@field backface_culling boolean
+---Add this much extra lighting when calculating texture color.
+---
+---Value < 0 disables light's effect on texture color.
+---
+---For faking self-lighting, UI style entities, or programmatic coloring in mods.
 ---@field glow integer
+---The name to display on the head of the object. By default empty.
+---
+---If the object is a player, a nil or empty nametag is replaced by the player's name.
+---
+---For all other objects, a nil or empty string removes the nametag.
+---
+---To hide a nametag, set its color alpha to zero. That will disable it entirely.
 ---@field nametag string
+---Text color of nametag.
 ---@field nametag_color ColorSpec
----@field nametag_bgcolor ColorSpec
+---Background color of nametag.
+---
+---`false` will cause the background to be set automatically based on user settings.
+---
+---Default: `false`
+---@field nametag_bgcolor ColorSpec|false
+---Same as infotext for nodes.
+---
+---Empty by default.
 ---@field infotext string
+---If `false`, never save this object statically.
+---
+---It will simply be deleted when the block gets unloaded.
+---
+---The get_staticdata() callback is never called then.
+---
+---Defaults to `true`.
 ---@field static_save boolean
+---Texture modifier to be applied for a short duration when object is hit.
 ---@field damage_texture_modifier string
+---Setting this to `false` disables diffuse lighting of entity.
 ---@field shaded boolean
+---Defaults to `true` for players, `false` for other entities.
+---
+---If set to `true` the entity will show as a marker on the minimap.
 ---@field show_on_minimap boolean
 
 ---Returns object properties.
@@ -193,7 +304,7 @@ function ObjectRef:get_properties() end
 ---@param object_properties object_properties
 function ObjectRef:set_properties(object_properties) end
 
----Returns true for players, false otherwise.
+---Returns `true` for players, `false` otherwise.
 ---@return boolean
 function ObjectRef:is_player() end
 
