f08d8d40e8b0c0c35ff487ffa1f9211b4a1751f0
AFCMS <afcm.contact@gmail.com>
2022-10-11 01:38:50 +0200

    Add some `@nodiscard` to `VoxelArea`

--- classes/voxelarea.lua
+++ classes/voxelarea.lua
@@ -1,31 +1,26 @@
 ---@meta
 
 ---@class VoxelArea
+---@field MinEdge Vector
+---@field MaxEdge Vector
+---@field ystride number
+---@field zstride number
 VoxelArea = {}
-
----@type Vector
-VoxelArea.MinEdge = nil
-
----@type Vector
-VoxelArea.MaxEdge = nil
-
----@type number
-VoxelArea.ystride = nil
-
----@type number
-VoxelArea.zstride = nil
 
 ---Create a new `VoxelArea`.
 ---@param o {MinEdge: Vector, MaxEdge: Vector}
 ---@return VoxelArea
+---@nodiscard
 function VoxelArea:new(o) end
 
 ---Returns a 3D vector containing the size of the area formed by `MinEdge` and `MaxEdge`.
 ---@return Vector
+---@nodiscard
 function VoxelArea:getExtent() end
 
 ---Returns the volume of the area formed by `MinEdge` and `MaxEdge`.
 ---@return number
+---@nodiscard
 function VoxelArea:getVolume() end
 
 ---Returns the index of an absolute position in a flat array starting at `1`.
@@ -36,6 +31,7 @@ function VoxelArea:getVolume() end
 ---@param y integer
 ---@param z integer
 ---@return integer
+---@nodiscard
 function VoxelArea:index(x, y, z) end
 
 ---Same functionality as `VoxelArea:index(x, y, z)` but takes a vector as param.
@@ -43,11 +39,13 @@ function VoxelArea:index(x, y, z) end
 ---As with `index(x, y, z)`, the components of `p` must be integers, and `p` is not checked for being inside the area volume.
 ---@param p Vector
 ---@return integer
+---@nodiscard
 function VoxelArea:indexp(p) end
 
 ---Returns the absolute position vector corresponding to index `i`.
 ---@param i integer
 ---@return Vector
+---@nodiscard
 function VoxelArea:position(i) end
 
 ---Check if (`x`,`y`,`z`) is inside area formed by `MinEdge` and `MaxEdge`.
@@ -55,15 +53,19 @@ function VoxelArea:position(i) end
 ---@param y number
 ---@param z number
 ---@return boolean
+---@nodiscard
 function VoxelArea:contains(x, y, z) end
 
 ---Same functionality as `VoxelArea:contains(x, y, z)` but takes a vector as param.
 ---@param p Vector
 ---@return boolean
+---@nodiscard
 function VoxelArea:containsp(p) end
 
 ---Same functionality as `VoxelArea:contains(x, y, z)` but takes an index as param.
 ---@param i integer
+---@return boolean
+---@nodiscard
 function VoxelArea:containsi(i) end
 
 ---Returns an iterator that returns indices.
@@ -75,9 +77,11 @@ function VoxelArea:containsi(i) end
 ---@param maxx integer
 ---@param maxy integer
 ---@param maxz integer
+---@nodiscard
 function VoxelArea:iter(minx, miny, minz, maxx, maxy, maxz) end
 
 ---Same functionality as `VoxelArea:iter(minx, miny, minz, maxx, maxy, maxz)` but takes vector as params.
 ---@param minp Vector
 ---@param maxp Vector
+---@nodiscard
 function VoxelArea:iterp(minp, maxp) end
