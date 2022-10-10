b1e4a284220121103d344f3523dee5f20e0577bf
AFCMS <afcm.contact@gmail.com>
2022-10-11 00:45:35 +0200

    Make pointed_thing use the right class syntax

--- classes/raycast.lua
+++ classes/raycast.lua
@@ -13,25 +13,18 @@ function Raycast(pos1, pos2, objects, liquids) end
 minetest.raycast = Raycast
 
 ---@class pointed_thing_raycast: pointed_thing
-local pointed_thing_raycast = {}
-
 ---The absolute world coordinates of the point on the selection box which is pointed at.
 ---
 ---May be in the selection box if the pointer is in the box too.
----@type Vector
-pointed_thing_raycast.intersection_point = nil
-
+---@field intersection_point Vector
 ---The ID of the pointed selection box (counting starts from 1).
----@type integer
-pointed_thing_raycast.box_id = nil
-
+---@field box_id integer
 ---Unit vector, points outwards of the selected selection box.
 ---
 ---This specifies which face is pointed at.
 ---
 ---Is a null vector `vector.zero()` when the pointer is inside the selection box.
----@type Vector
-pointed_thing_raycast.intersection_normal = nil
+---@field intersection_normal Vector
 
 ---@class Raycast
 local raycast = {}
--- types/pointed_thing.lua
+++ types/pointed_thing.lua
@@ -8,20 +8,11 @@
 ---* `{type="object", ref=ObjectRef}`
 ---    * Indicate a pointed ObjectRef
 ---@class pointed_thing
-local pointed_thing
-
 ---Indicates what is pointed at.
----@type '"nothing"'|'"node"'|'"object"'
-pointed_thing.type = nil
-
+---@field type '"nothing"'|'"node"'|'"object"'
 ---Node position behind the pointed face.
----@type Vector
-pointed_thing.under = nil
-
+---@field under Vector
 ---Node position in front of the pointed face.
----@type Vector
-pointed_thing.above = nil
-
+---@field above Vector
 ---Pointed ObjectRef.
----@type ObjectRef
-pointed_thing.ref = nil
+---@field ref ObjectRef
