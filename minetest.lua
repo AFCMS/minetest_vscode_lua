3292871bf19812c0b02b95cad400a46ff3b80b9e
AFCMS <afcm.contact@gmail.com>
2022-10-11 11:02:03 +0200

    State documentation is official for the minetest namespace

--- minetest.lua
+++ minetest.lua
@@ -2,7 +2,7 @@
 
 ---Minetest 7ffc0268 API
 ---
----[Documentation](https://github.com/minetest/minetest/blob/7ffc0268/doc/lua_api.txt)
+---[Official Documentation](https://github.com/minetest/minetest/blob/7ffc0268/doc/lua_api.txt)
 ---@class minetest
 minetest = {}
 
