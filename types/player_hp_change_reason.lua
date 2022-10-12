302243a7a669d80c611d18284309fcfe90007a85
AFCMS <afcm.contact@gmail.com>
2022-10-12 17:54:54 +0200

    Make PlayerHPChangeReason use right class syntax

--- types/player_hp_change_reason.lua
+++ types/player_hp_change_reason.lua
@@ -1,8 +1,6 @@
 ---@meta
 
 ---@class PlayerHPChangeReason
-local player_hp_change = {}
-
 ---Type of the change:
 ---* `set_hp`: A mod or the engine called `set_hp` without giving a type - use this for custom damage types.
 ---* `punch`: Was punched. `reason.object` will hold the puncher, or nil if none.
@@ -10,21 +8,14 @@ local player_hp_change = {}
 ---* `node_damage`: `damage_per_second` from a neighbouring node. `reason.node` will hold the node name or nil.
 ---* `drown`
 ---* `respawn`
----@type '"set_hp"'|'"punch"'|'"fall"'|'"node_damage"'|'"drown"'|'"respawn"'
-player_hp_change.type = nil
-
+---@field type '"set_hp"'|'"punch"'|'"fall"'|'"node_damage"'|'"drown"'|'"respawn"'
 ---The hp change origin.
----@type '"mod"'|'"engine"'
-player_hp_change.from = nil
-
+---@field from '"mod"'|'"engine"'
 ---**Damage type: punch**
 ---
 ---The puncher object or `nil` if there is none.
----@type ObjectRef?
-player_hp_change.object = nil
-
+---@field object ObjectRef?
 ---**Damage type: node_damage**
 ---
 ---The name of the damaging node
----@type string?
-player_hp_change.node = nil
+---@field node string?
