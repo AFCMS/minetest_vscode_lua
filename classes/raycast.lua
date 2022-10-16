826be88061215753f471fdab1bcc5cf02f7e978e
AFCMS <afcm.contact@gmail.com>
2022-10-17 00:51:19 +0200

    Fix `Raycast()` optional params

--- classes/raycast.lua
+++ classes/raycast.lua
@@ -5,8 +5,8 @@
 ---Create a Raycast
 ---@param pos1 Vector Start of the ray
 ---@param pos2 Vector End of the ray
----@param objects boolean If `false`, only nodes will be returned. Default is `true`.
----@param liquids boolean If `false`, liquid nodes (`liquidtype ~= "none"`) won't be returned. Default is `false`.
+---@param objects? boolean If `false`, only nodes will be returned. Default is `true`.
+---@param liquids? boolean If `false`, liquid nodes (`liquidtype ~= "none"`) won't be returned. Default is `false`.
 ---@return Raycast
 ---@nodiscard
 function Raycast(pos1, pos2, objects, liquids) end
