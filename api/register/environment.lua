094623d02666a7a9799d717a4924d61becf0cf0f
AFCMS <afcm.contact@gmail.com>
2022-10-19 21:17:20 +0200

    Move alias functions to environment functions

--- api/register/environment.lua
+++ api/register/environment.lua
@@ -1,5 +1,19 @@
 ---@meta
 
+---This adds an alias `alias` for the item called `original_name`
+---@param alias string
+---@param original_name string
+function minetest.register_alias(alias, original_name) end
+
+---Same as `minetest.register_alias` but will unregister the alias if it exists
+---@param alias string
+---@param original_name string
+function minetest.register_alias_force(alias, original_name) end
+
+---Map of registered aliases, indexed by name
+---@type table<string, string>
+minetest.registered_aliases = {}
+
 
 ---**Scatter**
 ---
