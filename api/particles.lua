d7ba82cc368af99b8d4c319d0fa7c220cc232637
AFCMS <afcm.contact@gmail.com>
2022-10-19 21:41:07 +0200

    Particle definition

---
+++ api/particles.lua
@@ -0,0 +1,51 @@
+---@meta
+
+---@class particle_definition
+---@field pos Vector
+---@field velocity Vector
+---@field acceleration Vector
+---Disappears after expirationtime seconds
+---@field expirationtime number
+---Scales the visual size of the particle texture.
+---
+---If `node` is set, size can be set to 0 to spawn a randomly-sized particle (just like actual node dig particles).
+---@field size number
+---If `true` collides with `walkable` nodes and, depending on the `object_collision` field, objects too.
+---@field collisiondetection boolean
+---If `true` particle is removed when it collides.
+---
+---Requires `collisiondetection = true` to have any effect.
+---@field collision_removal boolean
+---If `true` particle collides with objects that are defined as `physical = true,` and `collide_with_objects = true,`.
+---
+---Requires `collisiondetection = true` to have any effect.
+---@field object_collision boolean
+---If `true` faces player using y axis only.
+---@field vertical boolean
+---The texture of the particle.
+---@field texture string
+---Optional, if specified spawns particle only on the player's client.
+---@field playername string
+---Optional, specifies how to animate the particle texture.
+---@field animation tile_animation_definition
+---Optional, specify particle self-luminescence in darkness.
+---
+---Values 0-14.
+---@field glow integer
+---Optional, if specified the particle will have the same appearance as node dig particles for the given node.
+---
+---`texture` and `animation` will be ignored if this is set.
+---@field node node
+---Optional, only valid in combination with `node`.
+---
+---If set to a valid number 1-6, specifies the tile from which the particle texture is picked.
+---
+---Otherwise, the default behavior is used. (currently: any random tile)
+---@field node_tile integer
+
+---Add a particle with given definition.
+---
+---The overloaded definition is deprecated.
+---@overload fun(pos: Vector, velocity: Vector, acceleration: Vector, expirationtime: number, size: number, collisiondetection: boolean, texture: string, playername: string)
+---@param def particle_definition
+function minetest.add_particle(def) end
