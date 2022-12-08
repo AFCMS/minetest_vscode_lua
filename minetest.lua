9c3ec2669360859c80c08c54d766e76777147ed4
AFCMS <afcm.contact@gmail.com>
2022-12-08 16:31:14 +0100

    Make minetest global a table instead of a class

--- minetest.lua
+++ minetest.lua
@@ -3,9 +3,7 @@
 ---Minetest 5.6 API
 ---
 ---[Official Documentation](https://github.com/minetest/minetest/blob/5.6.0/doc/lua_api.txt)
----@class minetest
 minetest = {}
 
 ---@deprecated
----@type minetest
-core = {}
+core = minetest
